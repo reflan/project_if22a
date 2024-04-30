efe1774845136710f1149dede0b3ba3e22aadd16
reflan nuari <reflannuari@gmail.com>
2024-04-30 13:32:00 +0700

    commit

--- lib/config/asset.dart
+++ lib/config/asset.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Asset {
-  static Color colorPrimaryDark = Color.fromARGB(255, 2, 84, 100);
-  static Color colorPrimary = Color.fromARGB(255, 229, 124, 35);
-  static Color colorSecondary = Color.fromARGB(255, 232, 170, 6);
-  static Color colorAccent = Color.fromARGB(255, 115, 18, 32);
+  static Color colorPrimaryDark = Color.fromARGB(255, 115, 18, 32);
+  static Color colorPrimary = Color.fromARGB(255, 212, 36, 62);
+  static Color colorSecondary = Color.fromARGB(255, 255, 255, 0);
+  static Color colorAccent = Color.fromARGB(255, 13, 121, 91);
 }
--- lib/screen/admin/home_screen.dart
+++ lib/screen/admin/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_if22a/config/asset.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,7 +13,117 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       padding: EdgeInsets.all(10),
-      child: Text("Selamat datang di home screen"),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Asset.colorPrimary,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Image(
+              image: NetworkImage(
+                  'https://raw.githubusercontent.com/reflan/project_si21a/main/asset/images/Web-header-UTI-23.jpg'),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 25, bottom: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('User', Icons.people, 0),
+                    IconButton('Menu 2', Icons.mail, 0),
+                    IconButton('Menu 3', Icons.folder, 0),
+                    IconButton('Menu 4', Icons.person_add_alt_1, 0),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton('Menu 5', Icons.account_balance_wallet, 0),
+                    IconButton('Menu 6', Icons.edit_road_outlined, 0),
+                    IconButton('Menu 7', Icons.people, 0),
+                    IconButton('Menu 8', Icons.people, 0),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconButton extends StatelessWidget {
+  final String nameLabel;
+  final IconData iconLabel;
+  final int index;
+
+  IconButton(this.nameLabel, this.iconLabel, this.index);
+
+  List<Map> _fragment = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            // margin: EdgeInsets.only(bottom: 5),
+            child: Material(
+              borderRadius: BorderRadius.all(
+                Radius.circular(40),
+              ),
+              color: Colors.transparent,
+              child: InkWell(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(40),
+                ),
+                onTap: () {},
+                child: Container(
+                  // margin: EdgeInsets.all(5),
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Asset.colorPrimaryDark,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  child: Center(
+                    child: Stack(
+                      children: [
+                        Icon(
+                          iconLabel,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Text(
+              nameLabel,
+              style: TextStyle(fontSize: 14),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
